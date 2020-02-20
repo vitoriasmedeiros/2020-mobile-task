6aed82c5b5ab38253ffb7a50825e19a1a0dc0b0a
Leonardo Minora <leonardo.minora@gmail.com>
2020-02-20 09:06:02 -0300

    close #1

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Reac Web App!</Text>
       <Text>Meu primeiro app mobile!</Text>
     </View>
   );
